62a7c2be99d433bc338591e7a508a2dad65b4d22
kangyunsik <re12io@ajou.ac.kr>
2021-03-07 16:01:49 +0900

    + Permission check thread, when starting + Handling warnings

--- app/src/main/java/com/example/startupteam/MainActivity.java
+++ app/src/main/java/com/example/startupteam/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.startupteam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
@@ -14,16 +17,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] required_permission ={
+            Manifest.permission.ACCESS_FINE_LOCATION        // REQUIRED PERMISSIONs
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Checker checker = new Checker();                    // PERMISSION CHECKER THREAD
+        checker.start();
 
         EditText edit_id = findViewById(R.id.edit_id);
         EditText edit_password = findViewById(R.id.edit_pw);
@@ -40,24 +51,90 @@ public class MainActivity extends AppCompatActivity {
         Btn_PwFind.setText("비밀번호 찾기");
         RdBtn_consist.setText("로그인 상태 유지");
 
-        Intent intent = getIntent();
+        Btn_SignIn.setOnClickListener((View v) -> {
+            String id = edit_id.getText().toString();
+            String password = edit_password.getText().toString();
+            int response;
 
-        Btn_SignIn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String id = edit_id.getText().toString();
-                String password = edit_password.getText().toString();
+            Intent intent = new Intent(getApplicationContext(), MapActivity.class);     // After Sign in, Straight to Next Activity.
+            intent.putExtra("id",id);
+            intent.putExtra("password",password);
 
-                Intent intent = new Intent(getApplicationContext(), MapActivity.class);     // After Sign in, Straight to Next Activity.
 
-                //intent.putExtra("id",id);
-                //intent.putExtra("password",password);
+            response = checker.getResponse();
+
+            if(response == 1) {
                 startActivity(intent);
+            }else{
+                finish();
             }
         });
 
+//        Btn_SignIn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                String id = edit_id.getText().toString();
+//                String password = edit_password.getText().toString();
+//                int response;
+//
+//                Intent intent = new Intent(getApplicationContext(), MapActivity.class);     // After Sign in, Straight to Next Activity.
+//                intent.putExtra("id",id);
+//                intent.putExtra("password",password);
+//
+//
+//                response = checker.getResponse();
+//
+//                if(response == 1) {
+//                    startActivity(intent);
+//                }else{
+//                    finish();
+//                }
+//            }
+//        });
 
     }
 
+    private class Checker extends Thread {
+        private int response;
+
+        public Checker() {
+            // initializing
+        }
+
+        protected int getResponse() {
+            return this.response;
+        }
+
+        @Override
+        public void run() {
+            for(String permit : required_permission) {
+                int check = checkCallingOrSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (check == PackageManager.PERMISSION_DENIED) {
+                    requestPermissions(required_permission,0);
+                }
+            }
+            this.response = 1;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==0)
+        {
+            for(int grantResult : grantResults)
+            {
+                if(grantResult==PackageManager.PERMISSION_GRANTED){         // If allowed
+                    Toast.makeText(getApplicationContext(),"앱 권한을 허용했습니다.",Toast.LENGTH_LONG).show();
+                }
+                else {                                                      // If not allowed
+                    Toast.makeText(getApplicationContext(),"앱 권한이 필요합니다.",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
+    }
 
 }
